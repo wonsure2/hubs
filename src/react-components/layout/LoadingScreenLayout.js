0713ea63b2c8593bbad502d421ac9cb35d79a771
wangshuo.frontend <wangshuo.frontend@bytedance.com>
2022-09-13 22:59:34 +0800

    feat: 修改 mobile loading 样式

--- src/react-components/layout/LoadingScreenLayout.js
+++ src/react-components/layout/LoadingScreenLayout.js
@@ -2,13 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./LoadingScreenLayout.scss";
 import { Column } from "../layout/Column";
-import { AppLogo } from "../misc/AppLogo";
+// import { AppLogo } from "../misc/AppLogo";
+import loadingLogoImage from "../../assets/images/yuanjing/loading/loading-logo.png";
 
 export function LoadingScreenLayout({ center, bottom }) {
   return (
     <div className={styles.loadingScreenLayout}>
       <Column center padding gap="lg" className={styles.center} style={{ color: "#f9f9f9" }}>
-        <AppLogo className={styles.logo} style={{ width: 200, marginTop: -50 }} />
+        <img src={loadingLogoImage} className={styles.logo} />
         {center}
       </Column>
       {bottom && (
